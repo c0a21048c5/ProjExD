3b31cde67f2c2768eeb8c4d74d3dae2fc7257b71
Kobayashi Kota <c0a21048c5@edu.teu.ac.jp>
2022-06-14 15:44:34 +0900

    クイズプログラム

---
+++ ex01/quiz.py
@@ -0,0 +1,17 @@
+from random import randint
+def shutudai(Q1):
+    Q = ["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
+    print("問題"+"\n"+Q[Q1])
+    
+def kaito(Q1):
+    A = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子"]]
+    ans = input("答えるんだ>>>")
+    if ans in A[Q1]:
+        print("正解！！！")
+    else:
+        print("出直してこい")
+
+if __name__ == "__main__":
+    x1 = randint(0,2)
+    shutudai(x1)
+    kaito(x1)
