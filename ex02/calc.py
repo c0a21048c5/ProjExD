d1f56cc128a86175083dc3abc18fe2957652d4e3
Kobayashi Kota <c0a21048c5@edu.teu.ac.jp>
2022-06-21 16:46:29 +0900

    追加実装

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from random import randint
 
 if __name__ == "__main__":
     def button_click(event):
@@ -9,23 +10,45 @@ if __name__ == "__main__":
             ans = eval(text)
             
             return textbox.insert(tk.END,ans)
-            
+        
+        if check == "C":
+            return textbox.delete(0,tk.END)
+
+        if check == "R":
+                random = randint(0,10)
+                return textbox.insert(tk.END,random)
+
+        if check == "←":
+            sub = textbox.get()
+            return textbox.delete(len(sub)-1,tk.END)
+
+        if check == "x10":
+            sub = textbox.get()
+            textbox.delete(0,tk.END)
+            ans = float(sub)*10
+            return textbox.insert(tk.END,ans)
+        
+        if check == ".":
+            sub = textbox.get()
+            textbox.delete(0,tk.END)
+            ans = float(sub)/10
+            return textbox.insert(tk.END,ans)
+
         else:
             return textbox.insert(tk.END,check)
 
 
-
     root = tk.Tk()
-    root.geometry("300x700")
+    root.geometry("300x500")
     textbox = tk.Entry(root,justify="right",width=10,font=(("Times New Roman",40)))
     textbox.grid(row = 0,columnspan = 5)
     r,c = 1,0
 
-    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","=","/"]):
-        button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman",30))
+    for i,num in enumerate(["%",".","x10","R",9,8,7,"←",6,5,4,"C",3,2,1,"*","+",0,"/","="]):
+        button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman",20))
         button.grid(row=r,column=c)
         c += 1
-        if (i+1)%3 == 0:
+        if (i+1)%4 == 0:
             r += 1
             c = 0
         button.bind("<1>",button_click)
