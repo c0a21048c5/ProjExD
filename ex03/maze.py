3d821923a792a0e7b9858e109f6c0884d6fb7364
Kobayashi Kota <c0a21048c5@edu.teu.ac.jp>
2022-06-28 17:42:34 +0900

    改善プログラム #13

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,63 +1,49 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 import maze_maker as mm
-<<<<<<< HEAD
-=======
 import random
 import time
->>>>>>> add_func
 
 def key_down(event):
+    #キーが押されたときのイベントを取得
     global key
     key = event.keysym
     return key
 
 def key_up(event):
+    #キーを放したときの返り値を空白に設定
     global key
     key = ""
     return key
 
 def main_proc():
     global cx,cy,mx,my
-<<<<<<< HEAD
-=======
-    time_start = time.time()
->>>>>>> add_func
+    #各キーで行う動作を定義
     if key == "Up":
         my -= 1
-        if maze[my][mx] == 1:
+        if maze[my][mx] == 1: #床のみを移動するように設定
             my += 1
     if key == "Down":
         my += 1
-        if maze[my][mx] == 1:
+        if maze[my][mx] == 1: #上と同じ
             my -= 1
     if key == "Left":
         mx -= 1
-        if maze[my][mx] == 1:
+        if maze[my][mx] == 1: #上と同じ
             mx += 1
     if key == "Right":
         mx += 1
-        if maze[my][mx] == 1:
+        if maze[my][mx] == 1: #上と同じ
             mx -= 1
-<<<<<<< HEAD
-    cx,cy = mx * 100 + 50 , my * 100 + 50
-    canvas.coords("koukaton",cx,cy)
-    root.after(100,main_proc) 
 
-
-if __name__ == "__main__":
-    key = ""
-    mx,my = 1,1
-    cx,cy = mx * 100 + 50 , my * 100 + 50
-=======
-
-    cx,cy = mx * 100 + 50 , my * 100 + 50
-    canvas.coords("koukaton",cx,cy)
+    canvas.coords("koukaton",mx*100+50,my*100+50)
     jid = root.after(100,main_proc)
-
+    
+    #ゴール地点で行う処理を定義
     if mx == 13 and my == 7:
-        root.after_cancel(jid)
-        tkm.showinfo("クリア",f"ゲームクリアおめでとう！クリアタイムは{str(time_start-time_end)}秒")
+        time_end = time.time() #終了時間を取得
+        root.after_cancel(jid) #リアルタイム処理を中断
+        tkm.showinfo("クリア",f"ゲームクリアおめでとう！クリアタイムは{int(abs(time_start-time_end))}秒")
         return root.destroy()
 
 
@@ -67,38 +53,27 @@ if __name__ == "__main__":
     key = ""
     mx,my = 1,1
     cx,cy = mx * 100 + 50 , my * 100 + 50
+
+    #画像ファイル名のリストを定義
     tori_list = ["ex03/fig/0.png","ex03/fig/1.png","ex03/fig/2.png",
                  "ex03/fig/3.png","ex03/fig/4.png","ex03/fig/5.png",
                  "ex03/fig/6.png","ex03/fig/7.png","ex03/fig/8.png",
                  "ex03/fig/9.png"]
-    check = 0
->>>>>>> add_func
 
     root = tk.Tk()
     root.title("迷えるこうかとん")
-
     canvas = tk.Canvas(root,width=1500,
                        height=900,bg="black")
-<<<<<<< HEAD
+
     maze = mm.make_maze(15,9)
     mm.show_maze(canvas,maze)
-    koukaton = tk.PhotoImage(file="ex03/fig/1.png")
-=======
-    
-    maze = mm.make_maze(15,9)
-    mm.show_maze(canvas,maze)
-    tori = random.choice(tori_list)
+    tori = random.choice(tori_list) #リスト内からランダムでファイル名を取得
     koukaton = tk.PhotoImage(file=tori)
->>>>>>> add_func
     canvas.create_image(cx,cy,image=koukaton,tag="koukaton")
     canvas.pack()
 
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
     main_proc()
-<<<<<<< HEAD
-=======
-    time_end = time.time()
->>>>>>> add_func
-
+    
     root.mainloop()
