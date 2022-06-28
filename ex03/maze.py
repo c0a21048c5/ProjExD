f8ee311647e33a57d34d63f23e043ad11f20dd04
Kobayashi Kota <c0a21048c5@edu.teu.ac.jp>
2022-06-28 17:08:17 +0900

    ４限追加機能実装完了

---
+++ ex03/maze.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+import maze_maker as mm
+import random
+import time
+
+def key_down(event):
+    global key
+    key = event.keysym
+    return key
+
+def key_up(event):
+    global key
+    key = ""
+    return key
+
+def main_proc():
+    global cx,cy,mx,my
+    time_start = time.time()
+    if key == "Up":
+        my -= 1
+        if maze[my][mx] == 1:
+            my += 1
+    if key == "Down":
+        my += 1
+        if maze[my][mx] == 1:
+            my -= 1
+    if key == "Left":
+        mx -= 1
+        if maze[my][mx] == 1:
+            mx += 1
+    if key == "Right":
+        mx += 1
+        if maze[my][mx] == 1:
+            mx -= 1
+
+    cx,cy = mx * 100 + 50 , my * 100 + 50
+    canvas.coords("koukaton",cx,cy)
+    jid = root.after(100,main_proc)
+
+    if mx == 13 and my == 7:
+        root.after_cancel(jid)
+        tkm.showinfo("クリア",f"ゲームクリアおめでとう！クリアタイムは{str(time_start-time_end)}秒")
+        return root.destroy()
+
+
+
+if __name__ == "__main__":
+    time_start = time.time()
+    key = ""
+    mx,my = 1,1
+    cx,cy = mx * 100 + 50 , my * 100 + 50
+    tori_list = ["ex03/fig/0.png","ex03/fig/1.png","ex03/fig/2.png",
+                 "ex03/fig/3.png","ex03/fig/4.png","ex03/fig/5.png",
+                 "ex03/fig/6.png","ex03/fig/7.png","ex03/fig/8.png",
+                 "ex03/fig/9.png"]
+    check = 0
+
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+
+    canvas = tk.Canvas(root,width=1500,
+                       height=900,bg="black")
+    
+    maze = mm.make_maze(15,9)
+    mm.show_maze(canvas,maze)
+    tori = random.choice(tori_list)
+    koukaton = tk.PhotoImage(file=tori)
+    canvas.create_image(cx,cy,image=koukaton,tag="koukaton")
+    canvas.pack()
+
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
+    main_proc()
+    time_end = time.time()
+
+    root.mainloop()
